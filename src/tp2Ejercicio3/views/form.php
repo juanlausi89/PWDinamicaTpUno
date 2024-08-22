f49f57b087eebe27117ced194a24962c995b76cb
Juan Pablo Lausi <juanpablo_lausi@hotmail.com.ar>
2024-08-21 23:22:21 -0300

    css login

--- src/tp2Ejercicio3/views/form.php
+++ src/tp2Ejercicio3/views/form.php
@@ -11,20 +11,31 @@
     <script src="<?php echo constant('URL'); ?>src/assets/i18n/es.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <style>
+        .card{
+            -webkit-box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.75);
+            -moz-box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.75);
+            box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.75);
+            padding:2em ;
+            height: 20em;
+        }
+    </style>
+
 </head>
 <body>
-    <div class="container w-25 mt-5">
+    <div class="container w-25 mt-5 card">
         <form action="login" method="POST" data-parsley-validate="" id="#demo-form">
             <h2>Member Login</h2>
             <div class="mb-3">
-                <label for="username" class="form-label">Username</label>
-                <input type="text" class="form-control" id="username" name="username" required>
+                <input placeholder="Username" type="text" class="form-control" id="username" name="username" required>
             </div>
             <div class="mb-3">
-                <label for="password" class="form-label">Password</label>
-                <input type="text" class="form-control" id="password" name="password" required data-parsley-minlength="8"  data-parsley-user-distinto-password data-parsley-password-letras-numeros>
+                <input placeholder="Password" type="password" class="form-control" id="password" name="password" required data-parsley-minlength="8"  data-parsley-user-distinto-password data-parsley-password-letras-numeros>
             </div>
-            <button type="submit" class="btn btn-primary mt-3 mb-3">Login</button>
+            <div class="mb-3">
+                <button type="submit" style="width:100%;" class="btn btn-success mt-2">Login</button>
+            </div>
         </form> 
         <span class="mt-3"><?php echo  $this->d['resp']; ?></span>    
     </div>
